c724a5a463d402deb0a110cd2278ae1ed48b68d8
masudranainfo <mdmasude7@gmail.com>
2025-06-24 22:06:41 +0600

    hash password

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -3,6 +3,7 @@ import ApiError from "../../../error/ApiError";
 import IUser from "./user.interface";
 import User from "./user.model";
 import { generateUserId } from "./user.utils";
+import bcrypt from "bcrypt";
 
 const createUser = async (data: IUser): Promise<IUser | null> => {
   // auto genarated incremental id in 6 degit
@@ -14,6 +15,12 @@ const createUser = async (data: IUser): Promise<IUser | null> => {
   if (!data.password) {
     data.password = config.default_student_pass as string;
   }
+
+  // hash password
+  data.password = await bcrypt.hash(
+    data.password,
+    Number(config.bcrypt_salt_rounds)
+  );
 
   // Create a new user
   const user = await User.create(data);
--- src/config/index.ts
+++ src/config/index.ts
@@ -8,4 +8,5 @@ export default {
   port: process.env.PORT || 5000,
   mongodb_uri: process.env.MONGO_URI,
   default_student_pass: process.env.DEFAULT_STUDENT_PASS,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
 };
